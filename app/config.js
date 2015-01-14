2b889917a1c6be31915d71aefc0e88a7399482c8
Clark Feusier <cfeusier@gmail.com>
2015-01-13 19:10:23 -0800

    Remove old comments

--- app/config.js
+++ app/config.js
@@ -41,10 +41,6 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
-/************************************************************/
-// Add additional schema definitions below
-/************************************************************/
-
 db.knex.schema.hasTable('users').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('users', function (user) {
